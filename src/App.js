a3534232e37456418214bfeb5a881f17c2821f96
Ricardo Vieira <62380583+ricvigh@users.noreply.github.com>
2021-10-06 23:53:13 +0100

    getting graphGL API data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const WEATHERS_QUERY = `{
+  weathers{
+    cityName,
+    temperature,
+    description
+  }
+}`
 
 function App() {
+  const [weathers, setWeathers] = React.useState([]);
+  React.useEffect(() => {
+    fetch('http://localhost:3000/graphql/', {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ query:WEATHERS_QUERY })
+    }).then(response  => response.json())
+    .then(data => setWeathers(data.data.weathers))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Weather</h1>
+    {weathers.map((weather) => (
+      <li>{weather.cityName}, {weather.temperature}ºC, {weather.description}</li>
+    ))}
     </div>
   );
 }
